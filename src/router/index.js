5330f6a843312370a50d1918252aac7a49a1d65e
marcosgallardi <gallardimarcos98@gmail.com>
2024-07-25 16:07:17 -0300

    configuracion

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+// const userRouter = require("./user");
+// const orgRouter = require("./organization");
+
+// router.use("/user", userRouter);
+// router.use("/organization", orgRouter);
+
+module.exports = router;
